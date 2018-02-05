bc751978aef18a0936e4ad8739ee01fae010f723
F0ME <cryototrader@swissmail.com>
2018-02-06 01:01:47 +0200

    Initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define F0ME-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "F0MEURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
